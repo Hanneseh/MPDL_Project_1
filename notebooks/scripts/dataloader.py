1c77e66ce9ca2d4572e8338ceaa7bf8284ae0a79
Hannes Ehringfeld <hannes.ehringfeld@rub.de>
2023-04-19 14:38:09 +0200

    testing to work with torch dataloader

--- notebooks/scripts/dataloader.py
+++ notebooks/scripts/dataloader.py
@@ -8,3 +8,24 @@
         - make sure we have about equal amount of real and fake images in the train and test set
     - create a torch dataloader which can be used for training 
 '''
+
+
+import datasets
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+
+imagenette_dataset = datasets.load_dataset("frgfm/imagenette", "full_size", streaming=True, split="train")
+diffusion_db_dataset = datasets.load_dataset("poloclub/diffusiondb", "2m_all", streaming=True, split="train")
+
+# use with torch.utils.data.DataLoader
+imagenette_dataset = imagenette_dataset.with_format("torch")
+dataloader = DataLoader(imagenette_dataset, batch_size=4)
+
+EPOCHS = 2
+print("Training...")
+for epoch in range(EPOCHS):
+    running_loss = 0.0
+    for i, data in enumerate(tqdm(dataloader, desc=f"Epoch {epoch + 1} of {EPOCHS}", leave=True, ncols=80)):
+        inputs, labels = data
+        break
+    break
