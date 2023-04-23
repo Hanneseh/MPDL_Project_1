50e124fc068d53ff9c1a1a35fc3477d7d6cce35d
hakimguenther <hakimguenther@gmail.com>
2023-04-23 17:46:46 +0200

    second draft

---
+++ notebooks/scripts/build_custom_dataset.py
@@ -0,0 +1,42 @@
+# https://huggingface.co/docs/datasets/use_with_pytorch
+# imagenet_1k_dataset = datasets.load_dataset("imagenet-1k", split="train")
+
+import datasets
+import numpy as np
+
+imagenette_dataset = datasets.load_dataset("frgfm/imagenette", "full_size", split="train")
+diffusion_db_dataset = datasets.load_dataset("poloclub/diffusiondb", "2m_random_10k", split="train")
+
+# now add the "label" feature to the dataset
+# add the "label" feature to the dataset
+diffusion_labels = np.zeros(10000, dtype=np.int32)
+diffusion_labels += 1
+diffusion_db_dataset = diffusion_db_dataset.add_column("label", diffusion_labels)
+
+diffuision_columns_to_drop = ['prompt', 'seed', 'step', 'cfg', 'sampler', 'user_name', 'timestamp', 'image_nsfw', 'prompt_nsfw', 'width', 'height']
+
+# from diffusion_db_dataset drop all features except for "image" and "label" and 'width', 'height',
+diffusion_db_dataset = diffusion_db_dataset.remove_columns(diffuision_columns_to_drop)
+
+# for imagenette_dataset drop "label"
+imagenette_dataset = imagenette_dataset.remove_columns(['label'])
+imagenette_label = np.zeros(9469, dtype=np.int32)
+imagenette_dataset = imagenette_dataset.add_column("label", imagenette_label)
+
+# now concatenate the datasets
+dataset = datasets.concatenate_datasets([imagenette_dataset, diffusion_db_dataset])
+
+# now shuffle the dataset
+dataset = dataset.shuffle()
+
+# get dataset size
+dataset_size = len(dataset)
+
+# split into train validation and test (70/15/15)
+train_dataset = dataset.select(range(int(dataset_size * 0.7)))
+validation_dataset = dataset.select(range(int(dataset_size * 0.7), int(dataset_size * 0.85)))
+test_dataset = dataset.select(range(int(dataset_size * 0.85), dataset_size))
+
+train_dataset.save_to_disk("/Users/hannesehringfeld/ssd/Uni/DL_praktikum/MPDL_Project_1/notebooks/data/custom_image_classifier/train_dataset")
+test_dataset.save_to_disk("/Users/hannesehringfeld/ssd/Uni/DL_praktikum/MPDL_Project_1/notebooks/data/custom_image_classifier/test_dataset")
+validation_dataset.save_to_disk("/Users/hannesehringfeld/ssd/Uni/DL_praktikum/MPDL_Project_1/notebooks/data/custom_image_classifier/validation_dataset")
